64a0a42d388ddc59ad7ed0f8a496546de178802b
Vyacheslav Enis <venis@difane.com>
2017-12-10 22:18:49 +0200

    Dummy implementation for markdown render interface

--- pkg/unit/text_markdown.go
+++ pkg/unit/text_markdown.go
@@ -3,6 +3,7 @@ package unit
 // TextMarkdown represents unit which has some plain markdown content
 type TextMarkdown interface {
 	TextPlain
+	Render() string
 }
 
 // baseTextMarkdown represents default implementation of TextMarkdown interface
@@ -13,4 +14,8 @@ type baseTextMarkdown struct {
 // NewTextMarkdown creates new TextMarkdown unit with given title and data
 func NewTextMarkdown(title string, data string) TextMarkdown {
 	return &baseTextMarkdown{baseTextPlain: baseTextPlain{data: data, baseUnit: baseUnit{title: title}}}
+}
+
+func (t *baseTextMarkdown) Render() string {
+	panic("implement me")
 }
