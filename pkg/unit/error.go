e8e1905c0e30811981be87ab73a43fe16f5af85f
Vyacheslav Enis <venis@difane.com>
2017-12-17 15:34:22 +0200

    Added some docs

--- pkg/unit/error.go
+++ pkg/unit/error.go
@@ -2,6 +2,7 @@ package unit
 
 import "fmt"
 
+// JSONTypeError represents error when type received in JSON message is different from type of the target unit
 type JSONTypeError struct {
 	Expected, Actual string
 }
@@ -10,6 +11,7 @@ func (e JSONTypeError) Error() string {
 	return fmt.Sprintf("Unexpected unit type in json data received. Expected: %s, Actual: %s", e.Expected, e.Actual)
 }
 
+// TypeError represents error when type of some unit is unexpected at this point
 type TypeError struct {
 	Type string
 }
