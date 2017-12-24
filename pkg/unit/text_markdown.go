a356bbbc4fba85082eea3bb4b1a9fec0054e3dfc
Vyacheslav Enis <venis@difane.com>
2017-12-24 20:54:37 +0200

    Refactored text_markdown

--- pkg/unit/text_markdown.go
+++ pkg/unit/text_markdown.go
@@ -1,15 +1,8 @@
 package unit
 
-import (
-	"encoding/json"
-
-	"github.com/russross/blackfriday"
-)
-
 // TextMarkdown represents unit which has some plain markdown content
 type TextMarkdown interface {
 	TextPlain
-	Render() string
 }
 
 // baseTextMarkdown represents default implementation of TextMarkdown interface
@@ -18,47 +11,10 @@ type baseTextMarkdown struct {
 }
 
 // NewTextMarkdown creates new TextMarkdown unit with given title and data
-func NewTextMarkdown(title string, data string) TextMarkdown {
-	return &baseTextMarkdown{baseTextPlain: baseTextPlain{data: data, baseUnit: *newBaseUnit(title)}}
+func NewTextMarkdown() TextMarkdown {
+	return newBaseTextMarkdown()
 }
 
-func (u *baseTextMarkdown) Render() string {
-	return string(blackfriday.Run([]byte(u.data)))
-}
-
-func (u *baseTextMarkdown) Type() Type {
-	return TypeTextMarkdown
-}
-
-type baseTextMarkdownJSON struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
-	Type  Type   `json:"type"`
-	Data  string `json:"data"`
-}
-
-func (u *baseTextMarkdown) fromJSONStruct(j baseTextMarkdownJSON) error {
-	if j.Type != u.Type() {
-		return JSONTypeError{Expected: u.Type(), Actual: j.Type}
-	}
-	u.SetID(j.ID)
-	u.SetTitle(j.Title)
-	u.SetData(j.Data)
-
-	return nil
-}
-
-func (u *baseTextMarkdown) MarshalJSON() ([]byte, error) {
-	return json.Marshal(baseTextMarkdownJSON{ID: u.ID(), Title: u.Title(), Type: u.Type(), Data: u.Data()})
-}
-
-func (u *baseTextMarkdown) UnmarshalJSON(b []byte) error {
-	var jsonData baseTextMarkdownJSON
-	err := json.Unmarshal(b, &jsonData)
-
-	if err != nil {
-		return err
-	}
-
-	return u.fromJSONStruct(jsonData)
+func newBaseTextMarkdown() *baseTextMarkdown {
+	return &baseTextMarkdown{baseTextPlain: baseTextPlain{baseUnit: *newBaseUnit(TypeTextMarkdown)}}
 }
--- pkg/unit/text_markdown_test.go
+++ pkg/unit/text_markdown_test.go
@@ -3,66 +3,11 @@ package unit_test
 import (
 	"testing"
 
-	"io/ioutil"
-	"path/filepath"
-
-	"encoding/json"
-	"fmt"
-
 	"github.com/anothermemory/lib/pkg/unit"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewTextMarkdown(t *testing.T) {
-	u := unit.NewTextMarkdown("MyUnit", "MyData")
-	assert.NotNil(t, u.ID())
-	assert.NotEmpty(t, u.ID())
-	assert.Equal(t, "MyUnit", u.Title())
-	assert.Equal(t, "MyData", u.Data())
-}
-
-func TestTextMarkdown_Render(t *testing.T) {
-	cases := []string{
-		"Markdown Documentation - Basics",
-	}
-
-	for _, tc := range cases {
-		input := readFile(t, tc+".text")
-		expected := readFile(t, tc+".html")
-
-		actual := unit.NewTextMarkdown("MyUnit", input).Render()
-		assert.Equal(t, expected, actual, tc)
-	}
-}
-
-func TestTextMarkdown_MarshalJSON(t *testing.T) {
-	u := unit.NewTextMarkdown("MyUnit", "abc")
-
-	bytes, err := json.Marshal(u)
-	assert.NoError(t, err)
-	assert.JSONEq(t, fmt.Sprintf(`{"id": "%s", "title": "MyUnit", "data": "abc", "type": "text_markdown"}`, u.ID()), string(bytes))
-}
-
-func TestTextMarkdown_UnmarshalJSON(t *testing.T) {
-	u := unit.NewTextMarkdown("", "")
-
-	err := json.Unmarshal([]byte(`{"id": "123", "title": "MyUnit", "data": "abc", "type": "text_markdown"}`), &u)
-	assert.NoError(t, err)
-	assert.Equal(t, "123", u.ID())
-	assert.Equal(t, "MyUnit", u.Title())
-	assert.Equal(t, "abc", u.Data())
+	u := unit.NewTextMarkdown()
 	assert.Equal(t, unit.TypeTextMarkdown, u.Type())
-}
-
-func TestTextMarkdown_Type(t *testing.T) {
-	assert.Equal(t, unit.TypeTextMarkdown, unit.NewTextMarkdown("", "").Type())
-}
-
-func readFile(t *testing.T, name string) string {
-	filename := filepath.Join("testdata", name)
-	inputBytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		t.Errorf("Couldn't open '%s', error: %v\n", filename, err)
-	}
-	return string(inputBytes)
 }
