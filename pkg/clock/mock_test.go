057c2b5e51228c486c1787aa04175de171e7aa41
Vyacheslav Enis <venis@difane.com>
2017-12-25 15:26:09 +0200

    Added ability to pass multiple times to mock clock

--- pkg/clock/mock_test.go
+++ pkg/clock/mock_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 var dummyTime = time.Date(2017, 11, 24, 17, 0, 0, 0, time.Local)
+var dummySecondTime = time.Date(2017, 11, 25, 17, 0, 0, 0, time.Local)
 
 func TestNewMock(t *testing.T) {
 	c := clock.NewMock(dummyTime)
@@ -19,4 +20,12 @@ func TestNewMock(t *testing.T) {
 func TestMockClock_Now_SameValue(t *testing.T) {
 	c := clock.NewMock(dummyTime)
 	assert.Equal(t, c.Now(), c.Now())
+}
+
+func TestMockClock_Now_MultipleValues(t *testing.T) {
+	c := clock.NewMock(dummyTime, dummySecondTime)
+	assert.Equal(t, dummyTime, c.Now())
+	assert.Equal(t, dummySecondTime, c.Now())
+	assert.Equal(t, dummyTime, c.Now())
+	assert.Equal(t, dummySecondTime, c.Now())
 }
