f0e228bd41d4b3cde6574e2295206f57a0d3e6fb
Vyacheslav Enis <venis@difane.com>
2017-12-31 20:17:56 +0200

    Added tests for directory storage

--- pkg/storage/directory/storage_test.go
+++ pkg/storage/directory/storage_test.go
@@ -1,11 +1,16 @@
 package directory_test
 
 import (
+	"io/ioutil"
+	"os"
+	"path"
 	"testing"
 
 	"github.com/anothermemory/lib/pkg/storage"
 	"github.com/anothermemory/lib/pkg/storage/directory"
 	"github.com/anothermemory/lib/pkg/storage/test"
+	"github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDirectoryInMemoryStorage(t *testing.T) {
@@ -14,4 +19,14 @@ func TestDirectoryInMemoryStorage(t *testing.T) {
 
 func createDirectoryInMemoryStorage() storage.Storage {
 	return directory.NewDirectoryInMemoryStorage()
+}
+
+func TestDirectoryStorage(t *testing.T) {
+	dir, err := ioutil.TempDir("", "storage_directory_root")
+	assert.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	test.RunStorageTests(t, func() storage.Storage {
+		return directory.NewDirectoryStorage(path.Join(dir, uuid.NewV4().String()))
+	})
 }
